5ea86f5a4a0968e014ba71dfb469071cafbd8b99
Anneleen Swanepoel <anneleen.swanepoel@gmail.com>
2026-03-25 15:55:23 -0500

    Adding line number

--- dummy.py
+++ dummy.py
@@ -4,12 +4,12 @@ print('Opening dummy.txt')
 with open('dummy.txt', 'r') as in_stream:
     print('Opening output.txt')
     with open('output.txt', 'w') as out_stream:
-        for line in in_stream:
+        for line_index, line in enumerate (in_stream):
             line = line.strip()
             word_list = line.split()
             word_list.sort()
             for word in word_list:
-                out_stream.write(f'{word}\n')
+                out_stream.write(f'{line_index}\t{word}\n')
 print("Done!")
 print('dummy.txt is closed?', in_stream.closed)
 print('output.txt is closed?', out_stream.closed)
