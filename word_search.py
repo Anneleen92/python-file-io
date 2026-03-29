f956101a91fb08b68f068cbee827362f160bfc25
Anneleen Swanepoel <anneleen.swanepoel@gmail.com>
2026-03-29 16:52:31 -0500

    Adding the input file and creating the output file

--- word_search.py
+++ word_search.py
@@ -2,7 +2,9 @@
 
 import re
 
-if__name__ == "__main__":
+if __name__ == '__main__':
     herit_pattern_str = r'\w*herit\w*'
     herit_pattern = re.compile(herit_pattern_str, re.IGNORECASE)
 
+    input_file = "origin.txt"
+    output_file = "herit_results.txt"
